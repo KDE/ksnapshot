54e90fc69678f54b7ce5a42c760db3e9931e5ca6
Charles Samuels <charles@kde.org>
2000-01-22 08:06:34 +0000

    More KAboutData stuff.  Few tyops fixed. "KFax" I didn't mess with because it's in complete chaos.
    svn path=/trunk/kdegraphics/ksnapshot/; revision=38971

--- main.cpp
+++ main.cpp
@@ -3,15 +3,19 @@
 #include <kimgio.h>
 #include <klocale.h>
 #include <kcmdlineargs.h>
-
+#include <kaboutdata.h>
 
 static const char *description = 
 	I18N_NOOP("KDE Screenshot utility");
 
 int main(int argc, char **argv)
 {
-  KCmdLineArgs::init(argc, argv, "ksnapshot", description, KSNAPVERSION);
-
+  KAboutData aboutData( "ksnapshot", I18N_NOOP("KSnapshot"), 
+    KSNAPVERSION, description, KAboutData::GPL, 
+    "(c) 1997-1999, Richard J. Moore");
+  aboutData.addAuthor("Richard J. Moore",0, "rich@kde.org");
+  KCmdLineArgs::init( argc, argv, &aboutData );
+  
   KApplication app;
 
   kimgioRegister();
