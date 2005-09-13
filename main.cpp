9404c986a214a5680bd9478794715e5629fd42dc
Aaron J. Seigo <aseigo@kde.org>
2005-09-13 18:52:28 +0000

    add missing copyright notice
    svn path=/branches/KDE/3.5/kdegraphics/ksnapshot/; revision=460414

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
+/*
+  (c) Richard J. Moore 1997-2002
+  (c) Matthias Ettrich 2000
+  (c) Aaron J. Seigo 2002-2004
+  (c) Nadeem Hasan 2003
+  (c) Waldo Bastian 1999-2002
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or ( at your option ) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this library; see the file COPYING.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Steet, Fifth Floor,
+  Boston, MA 02110-1301, USA.
+*/
+
 #include <kapplication.h>
 #include <kimageio.h>
 #include <klocale.h>
