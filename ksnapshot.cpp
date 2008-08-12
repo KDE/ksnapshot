ce2502bda81139c204f859ffeb4f5cadf85f2250
Richard J. Moore <rich@kde.org>
2008-08-12 09:47:29 +0000

    - Fix saving of settings. There appears to have been a change in   behaviour in kdelibs that means simply writing the settings during the  closeEvent no longer causes things to hit the disk. Adding an explicit  sync solves the issue.
    CCMAIL: 164729-done@bugs.kde.org
    
    
    svn path=/trunk/KDE/kdegraphics/ksnapshot/; revision=845665

--- ksnapshot.cpp
+++ ksnapshot.cpp
@@ -410,6 +410,8 @@ void KSnapshot::closeEvent( QCloseEvent * e )
     KUrl url = filename;
     url.setPass( QString::null );	//krazy:exclude=nullstrassign for old broken gcc
     conf.writePathEntry("filename",url.url());
+
+    conf.sync();
     e->accept();
 }
 
