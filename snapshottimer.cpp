30ceed2fd26a61af4a319fb1f0a9f67860a683ce
Andrius Štikonas <stikonas@gmail.com>
2009-09-23 21:41:05 +0000

    i18n: Unify 2 strings.
    svn path=/trunk/KDE/kdegraphics/ksnapshot/; revision=1027357

--- snapshottimer.cpp
+++ snapshottimer.cpp
@@ -32,7 +32,8 @@ SnapshotTimer::SnapshotTimer() : QWidget(0)
 {
     setWindowFlags( Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::X11BypassWindowManagerHint);
     // The text is copied from paintEvent and the maximum number is used as %1 argument + margins
-    resize( fontMetrics().width( i18n("Snapshot will be taken in %1 seconds", 99) ) + 6 , fontMetrics().height() + 4 );
+    resize( fontMetrics().width( i18np("Snapshot will be taken in 1 second",
+                                       "Snapshot will be taken in %1 seconds", 99) ) + 6 , fontMetrics().height() + 4 );
     connect(&timer, SIGNAL(timeout()), this, SLOT(bell()));
 }
 
