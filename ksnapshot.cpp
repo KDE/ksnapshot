31bd9fa90839a7c691dc07dac0438cc01d018510
Aaron J. Seigo <aseigo@kde.org>
2010-07-20 18:44:37 +0000

    backport start up time fix by delaying the file increment check until save-time
    svn path=/branches/KDE/4.5/kdegraphics/ksnapshot/; revision=1152332

--- ksnapshot.cpp
+++ ksnapshot.cpp
@@ -164,11 +164,6 @@ KSnapshot::KSnapshot(QWidget *parent,  KSnapshotObject::CaptureMode mode )
     setIncludeDecorations(conf.readEntry("includeDecorations",true));
     filename = KUrl( conf.readPathEntry( "filename", QDir::currentPath()+'/'+i18n("snapshot")+"1.png" ));
 
-    // Make sure the name is not already being used
-    while(KIO::NetAccess::exists( filename, KIO::NetAccess::DestinationSide, this )) {
-        autoincFilename();
-    }
-
     connect( &grabTimer, SIGNAL( timeout() ), this, SLOT(  grabTimerDone() ) );
     connect( &updateTimer, SIGNAL( timeout() ), this, SLOT(  updatePreview() ) );
     QTimer::singleShot( 0, this, SLOT( updateCaption() ) );
@@ -216,6 +211,11 @@ void KSnapshot::resizeEvent( QResizeEvent * )
 
 void KSnapshot::slotSave()
 {
+    // Make sure the name is not already being used
+    while (KIO::NetAccess::exists( filename, KIO::NetAccess::DestinationSide, this )) {
+        autoincFilename();
+    }
+
     if ( save(filename, this) ) {
         modified = false;
         autoincFilename();
@@ -225,6 +225,11 @@ void KSnapshot::slotSave()
 
 void KSnapshot::slotSaveAs()
 {
+    // Make sure the name is not already being used
+    while (KIO::NetAccess::exists( filename, KIO::NetAccess::DestinationSide, this )) {
+        autoincFilename();
+    }
+
     QStringList mimetypes = KImageIO::mimeTypes( KImageIO::Writing );
     KFileDialog dlg( filename.url(), mimetypes.join(" "), this);
 
